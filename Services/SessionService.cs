9bcbc17eb845dae6d03444ce173eb50b3b268bd3
Mark Johnson <wedginet@gmail.com>
2025-07-24 07:47:45 -0500

    sessionservice

--- Services/SessionService.cs
+++ Services/SessionService.cs
@@ -1,9 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using MyWandTest.Models;
+using System.Collections.Generic;
 
 namespace MyWandTest.Services
 {
     public class SessionService
     {
+    public Guid CurrentActivityId { get; private set; } = Guid.NewGuid();
+
+        public List<ScannedTag> ScannedTagsLog { get; set; } = new();
+
+        public void AddScannedTag(string parsedTag, bool isDuplicate, bool isInAnimalTable)
+        {
+            var scannedTag = new ScannedTag
+            {
+                ActivityId = CurrentActivityId,
+                TagNumber = parsedTag,
+                IsDuplicate = isDuplicate,
+                IsInAnimalTable = isInAnimalTable
+            };
+
+            ScannedTagsLog.Add(scannedTag);
+        }
+
+        // Optionally a method to mark a tag as saved:
+        public void MarkTagAsSaved(Guid tagId)
+        {
+            var tag = ScannedTagsLog.FirstOrDefault(t => t.Id == tagId);
+            if (tag != null)
+            {
+                tag.IsSaved = true;
+            }
+        }
         public List<string> RawScans { get; private set; } = new();
         public List<string> ParsedScans { get; private set; } = new();
 
